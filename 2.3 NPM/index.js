06749303c6439df2bd5baface951215fc350164b
vssudarshan <vssudarshan89@gmail.com>
2026-03-23 15:39:51 +0530

    new tutorial to understand basics of NPM

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,15 @@
+import generateStupidName from "sillyName";
+
+// const sillyName = require("sillyname");
+
+
+const name = generateStupidName();
+
+console.log(`Name generated is ${name}`);
+
+
+import  {randomSuperhero} from "superheroes";
+
+const superName = randomSuperhero();
+
+console.log(`Super Hero ${superName} awakens...`);
